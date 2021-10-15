5bed15afa3e288f7e03ae8e8add3dfe5647cde2f
Hoang <hoangnguyen1247@gmail.com>
2021-10-15 13:23:30 +0700

    fix: add post script

--- buildScripts/postRelease.sh
+++ buildScripts/postRelease.sh
@@ -1,6 +1,7 @@
-CURRENT_BRANCH = $(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-CURREN_VERSION=$1
+echo "Post release"
+CURRENT_BRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+CURRENT_VERSION=$1
 git add package.json
 git add package-lock.json
-git commit -m "chore: post release "$CURREN_VERSION
+git commit -m "chore: release "$CURRENT_VERSION
 git push origin $CURRENT_BRANCH
