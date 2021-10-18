be1730be68358df61f3e306398ae6e15e6c1d3f5
Hoang <hoangnguyen1247@gmail.com>
2021-10-18 19:15:41 +0700

    fix: rename

--- src/paths.ts
+++ src/paths.ts
@@ -5,7 +5,7 @@
  */
 
 import debugLogger from "debug";
-import p from "path";
+import path from "path";
 import fs from "fs";
 
 const debug = debugLogger("pm2:paths");
@@ -16,13 +16,13 @@ function getDefaultPM2Home() {
     if (process.env.PM2_HOME) {
         PM2_ROOT_PATH = process.env.PM2_HOME;
     } else if (process.env.HOME && !process.env.HOMEPATH) {
-        PM2_ROOT_PATH = p.resolve(process.env.HOME, ".pm2t");
+        PM2_ROOT_PATH = path.resolve(process.env.HOME, ".pm2t");
     } else if (process.env.HOME || process.env.HOMEPATH) {
-        PM2_ROOT_PATH = p.resolve(process.env.HOMEDRIVE, process.env.HOME || process.env.HOMEPATH, ".pm2t");
+        PM2_ROOT_PATH = path.resolve(process.env.HOMEDRIVE, process.env.HOME || process.env.HOMEPATH, ".pm2t");
     } else {
         console.error("[PM2][Initialization] Environment variable HOME (Linux) or HOMEPATH (Windows) are not set!");
         console.error("[PM2][Initialization] Defaulting to /etc/.pm2t");
-        PM2_ROOT_PATH = p.resolve("/etc", ".pm2t");
+        PM2_ROOT_PATH = path.resolve("/etc", ".pm2t");
     }
 
     debug("pm2 home resolved to %s", PM2_ROOT_PATH, process.env.HOME);
@@ -32,7 +32,7 @@ function getDefaultPM2Home() {
 export default function (PM2_HOME) {
     let has_node_embedded = false;
 
-    if (fs.existsSync(p.resolve(__dirname, "./node")) === true) {
+    if (fs.existsSync(path.resolve(__dirname, "./node")) === true) {
         has_node_embedded = true;
     }
 
@@ -44,32 +44,32 @@ export default function (PM2_HOME) {
         PM2_HOME: PM2_HOME,
         PM2_ROOT_PATH: PM2_HOME,
 
-        PM2_CONF_FILE: p.resolve(PM2_HOME, "conf.js"),
-        PM2_MODULE_CONF_FILE: p.resolve(PM2_HOME, "module_conf.json"),
+        PM2_CONF_FILE: path.resolve(PM2_HOME, "conf.js"),
+        PM2_MODULE_CONF_FILE: path.resolve(PM2_HOME, "module_conf.json"),
 
-        PM2_LOG_FILE_PATH: p.resolve(PM2_HOME, "pm2t.log"),
-        PM2_PID_FILE_PATH: p.resolve(PM2_HOME, "pm2t.pid"),
+        PM2_LOG_FILE_PATH: path.resolve(PM2_HOME, "pm2t.log"),
+        PM2_PID_FILE_PATH: path.resolve(PM2_HOME, "pm2t.pid"),
 
-        PM2_RELOAD_LOCKFILE: p.resolve(PM2_HOME, "reload.lock"),
+        PM2_RELOAD_LOCKFILE: path.resolve(PM2_HOME, "reload.lock"),
 
-        DEFAULT_PID_PATH: p.resolve(PM2_HOME, "pids"),
-        DEFAULT_LOG_PATH: p.resolve(PM2_HOME, "logs"),
-        DEFAULT_MODULE_PATH: p.resolve(PM2_HOME, "modules"),
-        PM2_IO_ACCESS_TOKEN: p.resolve(PM2_HOME, "pm2-io-token"),
-        DUMP_FILE_PATH: p.resolve(PM2_HOME, "dump.pm2t"),
-        DUMP_BACKUP_FILE_PATH: p.resolve(PM2_HOME, "dump.pm2t.bak"),
+        DEFAULT_PID_PATH: path.resolve(PM2_HOME, "pids"),
+        DEFAULT_LOG_PATH: path.resolve(PM2_HOME, "logs"),
+        DEFAULT_MODULE_PATH: path.resolve(PM2_HOME, "modules"),
+        PM2_IO_ACCESS_TOKEN: path.resolve(PM2_HOME, "pm2-io-token"),
+        DUMP_FILE_PATH: path.resolve(PM2_HOME, "dump.pm2t"),
+        DUMP_BACKUP_FILE_PATH: path.resolve(PM2_HOME, "dump.pm2t.bak"),
 
-        DAEMON_RPC_PORT: p.resolve(PM2_HOME, "rpc.sock"),
-        DAEMON_PUB_PORT: p.resolve(PM2_HOME, "pub.sock"),
-        INTERACTOR_RPC_PORT: p.resolve(PM2_HOME, "interactor.sock"),
+        DAEMON_RPC_PORT: path.resolve(PM2_HOME, "rpc.sock"),
+        DAEMON_PUB_PORT: path.resolve(PM2_HOME, "pub.sock"),
+        INTERACTOR_RPC_PORT: path.resolve(PM2_HOME, "interactor.sock"),
 
-        INTERACTOR_LOG_FILE_PATH: p.resolve(PM2_HOME, "agent.log"),
-        INTERACTOR_PID_PATH: p.resolve(PM2_HOME, "agent.pid"),
-        INTERACTION_CONF: p.resolve(PM2_HOME, "agent.json5"),
+        INTERACTOR_LOG_FILE_PATH: path.resolve(PM2_HOME, "agent.log"),
+        INTERACTOR_PID_PATH: path.resolve(PM2_HOME, "agent.pid"),
+        INTERACTION_CONF: path.resolve(PM2_HOME, "agent.json5"),
 
         HAS_NODE_EMBEDDED: has_node_embedded,
-        BUILTIN_NODE_PATH: has_node_embedded === true ? p.resolve(__dirname, "./node/bin/node") : null,
-        BUILTIN_NPM_PATH: has_node_embedded === true ? p.resolve(__dirname, "./node/bin/npm") : null,
+        BUILTIN_NODE_PATH: has_node_embedded === true ? path.resolve(__dirname, "./node/bin/node") : null,
+        BUILTIN_NPM_PATH: has_node_embedded === true ? path.resolve(__dirname, "./node/bin/npm") : null,
     };
 
     // allow overide of file paths via environnement
