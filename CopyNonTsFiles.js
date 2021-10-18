bb99c0596083ed9dfe5f043ad8b9c4d919da44cc
Hoàng Ng <hoang.nguyen1247@gmail.com>
2021-10-18 20:27:07 +0700

    fix: copy motd

--- CopyNonTsFiles.js
+++ CopyNonTsFiles.js
@@ -54,43 +54,6 @@ copyDirSync(
     }
 );
 
-fs.ensureDirSync("./lib");
-copyDirSync(
-    snapCoreFolders,
-    "./src/motd",
-    "./lib/motd",
-    {
-        filter: function(stat, filepath, filename) {
-            // do not want copy .git directories
-            if (stat === "directory" && filename === "node_modules") {
-                return false;
-            }
-
-            if (stat === "directory" && filename === ".git") {
-                return false;
-            }
-
-            if (filename === "yarn.lock") {
-                return false;
-            }
-
-            if (stat === "directory" && filename === "examples") {
-                return false;
-            }
-
-            if (stat === "directory" && filename === "dist") {
-                return false;
-            }
-
-            if (stat === "directory" && filename === "lib") {
-                return false;
-            }
-
-            return true; // remind to return a true value when file check passed.
-        },
-    }
-);
-
 // const snapDomFolders = ['assets', 'components', 'utils'];
 // copyDirSync(
 //   snapDomFolders,
